9863c1bd48ef0ed5da4574517a52e0d60be42c51
ncasasco <ncasasco@gmail.com>
2022-11-16 05:36:18 -0300

    Added DataAccess project for db implementation

---
+++ 236894/DataAccess/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using BusinessLogic;
+using System.Text;
+
+namespace BusinessLogic
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
